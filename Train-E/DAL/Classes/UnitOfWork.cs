21b9f2d255619fc986f036e06f2eb79614f8a7d8
Patryk Wyżgowski <patrykwyzgowskig9@wp.pl>
2017-11-19 00:18:01 +0100

    Added Unit Of Work and its interface

---
+++ Train-E/DAL/Classes/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Train_E.DAL.Classes
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        private TrainEContext context = new TrainEContext();
+
+        private Dictionary<Type, object> repositories = new Dictionary<Type, object>();
+
+        public IGenericRepository<T> GetRepository<T>() where T : class
+        {
+            if (repositories.Keys.Contains(typeof(T)) == true)
+            {
+                return repositories[typeof(T)] as IGenericRepository<T>;
+            }
+            IGenericRepository<T> repo = new GenericRepository<T>(db);
+            repositories.Add(typeof(T), repo);
+            return repo;
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Train-E/DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train_E.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<T> GetRepository<T>() where T : class;
+        void Save();
+    }
+}
